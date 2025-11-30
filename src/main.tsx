78f3b3355325cde5386e25bbc0732b9812ae56c1
SeyeonAhn <ahnse0424@gmail.com>
2025-11-30 11:50:33 +0900

    feat: 백엔드 API 호출 및 Mixpanel distinctId 통일

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { initMixpanel } from './utils/mixpanel'
+import { initMixpanel, identifyUserFromStorage } from './utils/mixpanel'
 
 // Mixpanel 초기화
 initMixpanel()
+
+// 저장된 userId로 identify (백엔드와 distinctId 통일)
+identifyUserFromStorage()
 
 createRoot(document.getElementById('root')!).render(
   <App />
